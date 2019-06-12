bfda21e812eb983ae82347d605cc7624dc331b75
Denis <komarovec1@gmail.com>
2019-06-12 21:06:15 +0200

    Compet preparation 2

--- CodeSnippets/src/main.cpp
+++ CodeSnippets/src/main.cpp
@@ -6,19 +6,51 @@
 #define LCDWIDTH 20
 #define LCDHEIGHT 4
 
-//Constants
+//-------------------------------
+//---------- Constants ----------
+//-------------------------------
+
+//Buttons
 const int redBtn = 3; //INT 5
 const int blueBtn = 2; //INT 4
 const int greenBtn = 18; //INT 3
 const int blackBtn = 19; //INT 2
+
+//Two-pos switches
 const int switch1 = 53; //SWITCH1
 const int switch2 = 49; //SWITCH2
 const int powerPin = 51; //PP
 
+//Leds
 const int redLed = 13;
 const int greenLed  = 12;
 const int blueLed  = 11;
 const int yellowLed = 10;
+
+//PIR
+const int pir = 29;
+
+//Rotary encoder
+const int dt = 27;
+const int clk = 26;
+const int reBtn = 28;
+bool cw = false;
+
+//Pos meter
+const int pos = 35;
+
+//RGB Led
+const int rgbRed = 32; 
+const int rgbGreen = 33;
+const int rgbBlue = 34;
+
+//--------------------------------------
+//---------- END OF CONSTANTS ----------
+//--------------------------------------
+
+//--------------------------------------
+//---------- VARS / OBJECTS ------------
+//--------------------------------------
 
 //LCD Type
 LiquidCrystal_I2C lcd(0x27, 20, 4);
@@ -45,10 +77,23 @@ unsigned long blueBtnWait = 0;
 unsigned long greenBtnWait = 0;
 unsigned long blackBtnWait = 0;
 
-//Prog 1
 unsigned long animWait = 0;
 int animDelay = 200;
 
+//Rotary encoder vars
+int dtLaststate = 0;
+int clkLaststate = 0;
+int encoderPos = 0;
+
+//PIR vars
+int pirLaststate = 0;
+
+//Pos vars
+int posLaststate = 0;
+
+//---------------------------------
+//---------- ACTIVE CODE ----------
+//---------------------------------
 
 //Functions declaration
 void redBtnCallback();
@@ -154,6 +199,22 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(greenBtn), greenBtnCallback, RISING);
   attachInterrupt(digitalPinToInterrupt(blackBtn), blackBtnCallback, RISING);
 
+  //Rotary encoder
+  pinMode(dt, INPUT);
+  pinMode(clk, INPUT);
+  pinMode(reBtn, INPUT);
+
+  //PIR
+  pinMode(pir, INPUT);
+
+  //RGB LED
+  pinMode(rgbRed, OUTPUT);
+  pinMode(rgbGreen, OUTPUT);
+  pinMode(rgbBlue, OUTPUT);
+
+  //POS
+  pinMode(pos, INPUT_PULLUP);
+
   //LCD init
   lcd.init();
   lcd.backlight();
@@ -166,6 +227,52 @@ void setup() {
 void loop() {
   //lcd.scrollLoop();
 
+  //Rotary encoder logic
+  int dtNow = digitalRead(dt);
+  int clkNow = digitalRead(clk);
+  if(dtNow != dtLaststate) {
+    dtLaststate = dtNow;
+    if(clkNow == dtNow) {
+      encoderPos++;
+      cw = true;
+      digitalWrite(rgbRed, HIGH);
+    }
+    else {
+      encoderPos--;
+      cw = false;
+      digitalWrite(rgbRed, LOW);
+    }
+
+    lcd.setCursor(12,0);
+    lcd.print("       ");
+    lcd.setCursor(0,0);
+    lcd.print("Encoder pos: "+String(encoderPos));
+  }
+
+  //PIR Logic
+  int pirState = digitalRead(pir);
+  if(pirState != pirLaststate) {
+    pirLaststate = pirState;
+    lcd.setCursor(0,1);
+    lcd.print("          ");
+    lcd.setCursor(0,1);
+    lcd.print("PIR: ");
+    lcd.print(pirState);
+    digitalWrite(rgbBlue, pirState);
+  }
+
+  //Position sensor
+  int posState = digitalRead(pos);
+  if(posState != posLaststate) {
+    posLaststate = posState;
+    lcd.setCursor(0,2);
+    lcd.print("          ");
+    lcd.setCursor(0,2);
+    lcd.print("Pos: ");
+    lcd.print(posState);
+    digitalWrite(rgbGreen, posState);
+  }
+
   //Check switches
   int switch1State = digitalRead(switch1);
   int switch2State = digitalRead(switch2);
@@ -174,11 +281,13 @@ void loop() {
     lastSwitch2State = switch2State;
   }  
 
+/*  
   if(animWait < millis()) {
     animWait = millis() + animDelay;
-    lcd.setCursor(0,0);
-    lcd.print("123456789123456789");
+    lcd.setCursor(1,0);
+    lcd.print("Animovany text juu toci se jak petr");
   }
+*/
 
   checkInter();
 
